2fae6705bb26eb1ad82b95b6328766e004165215
Hyunsujk <hyunsujk@gmail.com>
2020-07-12 15:17:06 -0500

    created div wrapping letter

--- src/components/WordDisplay/WordDisplay.js
+++ src/components/WordDisplay/WordDisplay.js
@@ -3,14 +3,14 @@ import "./WordDisplay.css";
 import Box from "../WordDisplay/Box";
 
 function WordDisplay(props) {
-  console.log(props.hitsArray);
+  console.log("props hit", props.hitsArray);
   return (
     <div className="word-display-container">
       <div className="box-container">
         {props.wordCharArray.map((char, index) => {
           if (props.hitsArray.indexOf(char) !== -1) {
             return (
-              <div>
+              <div className="letter-display">
                 <h1>{char}</h1>
               </div>
             );
